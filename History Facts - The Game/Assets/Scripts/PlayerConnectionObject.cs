f2ba414cec89cffcdaff8e2920a025c588ff8fd3
RuFFuS4 <rgr93.4@outlook.es>
2018-05-17 19:48:36 +0200

    continues with network conection

---
+++ History Facts - The Game/Assets/Scripts/PlayerConnectionObject.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class PlayerConnectionObject : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Is this actually my own local PlayerConnectionObject?
+        if (isLocalPlayer == false) {
+            //This object belongs to another player.
+            return;
+        }
+        //Since the PlayerConnectionObject is invisible and not part of the world, give me something physical to move around!
+        //Instantiate() only creates an object on the LOCAL COMPUTER even if it has a NetworkIdentity is still will NOT exist on the network (and therefore not on any other client)
+        //UNLESS NetworkServer.Spawn() is called on this object.
+
+        //Instantiate(PlayerUnitPrefab);
+
+        //Command (politely) the server to SPAWN out unit
+        CmdSpawnMyUnit();
+	}
+    public GameObject PlayerUnitPrefab;
+	// Update is called once per frame
+	void Update () {
+       /* if (isLocalPlayer == false)
+        {
+            //This object belongs to another player.
+            return;
+        }
+
+        //PRUEBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        if (Input.touchCount >0) {
+            CmdMoveup();
+        }*/
+    }
+
+    /*COMMANDS*/
+    //Commands are special functions that ONLY get executed on the server.
+    /*ALL commands should have the word Cmd*/
+    GameObject myPlayerUnit;
+
+    [Command]
+    void CmdSpawnMyUnit() {
+        //We are guaranteed to be on the server rigth now.
+        GameObject go = Instantiate(PlayerUnitPrefab);
+        myPlayerUnit = go;
+        /*go.GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient);*/
+        //Now that the object exist on the server, propagate it to all the clients (and also wire up the NetworkIdentity)
+        NetworkServer.SpawnWithClientAuthority(go,connectionToClient);
+    }
+
+    //PRUEBAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    [Command]
+    void CmdMoveup() {
+        if (myPlayerUnit == null) {
+            return;
+        }
+        myPlayerUnit.transform.Translate(0,1,0);
+    }
+}
---
+++ History Facts - The Game/Assets/Scripts/PlayerUnit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+//A PlayerUnit is a unit controlled by a player
+// This could be a character in a FPS, a zerling in a RTS or a scout in a TBS
+
+public class PlayerUnit : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //How do I verify that I am allowed to mess around with this object?
+        if (hasAuthority == false)
+        {
+            return;
+        }
+        //PRUEBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        if (Input.touchCount > 0)
+        {
+            this.transform.Translate(0,1,0);
+            
+        }
+        //This function runs on ALL PlayerUnits -- not just the ones that I own.
+    }
+}
